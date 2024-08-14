3626fe19ea85c72977aeecbeaca2726069918fe9
nitinsehrawat020 <nitinsehrawat020@gmail.com>
2024-08-14 14:00:58 +0530

    14/aug/2024

--- 07-usepopcorn/starter/usepopcor/src/index.js
+++ 07-usepopcorn/starter/usepopcor/src/index.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { useState } from "react";
+
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [rating, setRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setRating} />
+      <p>the movuie was rated {rating} star</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      // maxRating={5}
+      messages={["terriable", "bad", "okay", "good", "amazing"]}
+      defaultRating={3}
+    />
     {/* <App /> */}
+    <Test />
   </React.StrictMode>
 );
