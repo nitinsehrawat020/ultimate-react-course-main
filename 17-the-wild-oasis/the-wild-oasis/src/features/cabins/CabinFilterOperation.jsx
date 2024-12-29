e7671767332a48e4d015d72f6200a5e63eb6a1bd
nitinsehrawat020 <nitinsehrawat020@gmail.com>
2024-12-29 22:55:35 +0530

    12-29-2024

--- 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CabinFilterOperation.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CabinFilterOperation.jsx
@@ -17,8 +17,8 @@ function CabinFilterOperation() {
         options={[
           { value: "name-asc", label: "Sort by name(A-Z)" },
           { value: "name-desc", label: "Sort by name(Z-A)" },
-          { value: "RegularPrice-asc", label: "Sort by price(Low first)" },
-          { value: "RegularPrice-desc", label: "Sort by price(High first)" },
+          { value: "regularPrice-asc", label: "Sort by price(Low first)" },
+          { value: "regularPrice-desc", label: "Sort by price(High first)" },
           { value: "maxCapacity-asc", label: "Sort by capacity(low first)" },
           {
             value: "maxCapacity-desc",
--- 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
@@ -11,8 +12,8 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  //1)filter
   const filterValue = searchParams.get("discount") || "all";
-  console.log(filterValue);
 
   let filterCabins;
 
@@ -21,6 +22,18 @@ function CabinTable() {
     filterCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filterCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // 2)sort by
+  // console.log(filterCabins);
+
+  const sortby = searchParams.get("sortBy") || "name-asc";
+  const [field, direction] = sortby.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabin = filterCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
+  if (!sortedCabin.length) return <Empty resource="cabin" />;
 
   return (
     <Menus>
@@ -34,7 +47,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filterCabins}
+          data={sortedCabin}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
